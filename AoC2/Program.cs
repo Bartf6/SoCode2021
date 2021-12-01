fec1f4eb5ede99fcffc8917184174ef05ce45739
Bart Fransen <bart.fransen@sogeti.com>
2021-12-01 10:51:31 +0100

    dag 1

---
+++ AoC2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AoC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            List<int> DepthList = new List<int>();
+            int Increases = 0;
+            int SumA = 0;
+            int SumB = 0;
+
+            foreach (string line in System.IO.File.ReadLines(@"C:\Users\bfransen\source\repos\Advent of Code\Advent of Code\input.txt"))
+            {
+                DepthList.Add(Int32.Parse(line));
+                counter++;
+            }
+
+            for (int i = 0; i < counter - 1; i++)
+            {
+                if(i+3 == counter)
+                {
+                    break;
+                }
+
+                SumA = DepthList[i] + DepthList[i + 1] + DepthList[i + 2];
+                SumB = DepthList[i + 1] + DepthList[i + 2] + DepthList[i + 3];
+
+                if (SumA < SumB)
+                {
+                    Increases++;
+                }
+            }
+
+            Console.WriteLine("Amount of Increases: {0}.", Increases);
+            Console.ReadLine();
+        }
+    }
+}
