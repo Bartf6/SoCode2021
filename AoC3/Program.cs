a914dfd089ff0c4023da55515e6ce09b6143cd29
Bart Fransen <bart.fransen@sogeti.com>
2021-12-02 09:54:16 +0100

    Dag 2

---
+++ AoC3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AoC3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Depth = 0;
+            int Horizontal = 0;
+
+            foreach(string line in System.IO.File.ReadLines(@"C:\Users\bfransen\source\repos\Advent of Code\AoC3\input.txt"))
+            {
+                string s = line;
+                string[] subs = s.Split(' ');
+                switch (subs[0])
+                {
+                    case "forward":
+                        Horizontal = Horizontal + Int32.Parse(subs[1]);
+                        break;
+                    case "up":
+                        Depth = Depth - Int32.Parse(subs[1]);
+                        break;
+                    case "down":
+                        Depth = Depth + Int32.Parse(subs[1]);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine("Diepte: " + Depth);
+            Console.WriteLine("Horizontaal: " + Horizontal);
+            Console.WriteLine("Vermenigvuldiging: " + (Horizontal * Depth));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AoC4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AoC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Depth = 0;
+            int Horizontal = 0;
+            int Aim = 0;
+
+            foreach (string line in System.IO.File.ReadLines(@"C:\Users\bfransen\source\repos\Advent of Code\AoC4\input.txt"))
+            {
+                string s = line;
+                string[] subs = s.Split(' ');
+                switch (subs[0])
+                {
+                    case "forward":
+                        Horizontal = Horizontal + Int32.Parse(subs[1]);
+                        Depth = Depth + (Int32.Parse(subs[1]) * Aim);
+                        break;
+                    case "up":
+                        Aim = Aim - Int32.Parse(subs[1]);
+                        break;
+                    case "down":
+                        Aim = Aim + Int32.Parse(subs[1]);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine("Diepte: " + Depth);
+            Console.WriteLine("Horizontaal: " + Horizontal);
+            Console.WriteLine("Mikpunt: " + Aim);
+            Console.WriteLine("Vermenigvuldiging: " + (Horizontal * Depth));
+            Console.ReadLine();
+        }
+    }
+}
